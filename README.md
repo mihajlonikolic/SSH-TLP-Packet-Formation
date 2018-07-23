ff433ce141d6a8fcb475a13e144c62d60b1482b7
mihajlonikolic <nikolic94.mn@gmail.com>
2018-07-23 22:19:42 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# SSH-TLP-Packet-Formation
+### Introduction
+
+[Go to VM](http://{{ vm_ip('main') }}:8000/)
+
+This scenario should help students to understand how packet exchange by using SSH protocol actually works.
+
+![SSH Transport Layer Protocol Packet Formation]( static/SSH.png)
+
+SSH Transport Layer Protocol Packet Formation [1](#fn1)
+
+The scenario is a console application that provides the string preparation for safe sending via a computer network in a manner similar to the way in which packet is formed in the _SSH Transport Layer Protocol_ step by step. _Compression_ and _seq#_ (schema [1](#fn1)) are not implemented. The program is a string of arbitrary lengths that are being prepared for sending, and keys for encryption KE and KMAC for integrity protection. The pre-encoding string must be formatted by concatenating the following fields (see schema [2](#fn2)):
+
+*   **pktl** \- packet length in bytes, without pktl and MAC fields (_4 bytes_)
+*   **pdl** \- the length of the padding, adding that the total length of the given string would be an integer of the block size required by the block encoder (_1 byte_)
+*   **payload** \- set text ( _\> 1 byte_)
+*   **padding** \- add-on at the end of the given text so that the total length of the given string is an integer of the block size that requires a block encoder ( _< 8 bytes_)
+
+The formatted string is encrypted by using the **DES algorithm** in **CBC** mode. IV is generated as a random number. Formation of MAC by using HMAC-SHA1. CBC (_Cipher Block Chaining_) mode (schema below) is implemented directly in code step by step, without using any additional functions.
+
+![Cipher Block Chaining (CBC) mode encryption]( static/CBC.jpg)
+
+Cipher Block Chaining (CBC) mode encryption [2](#fn2)
+
+The student can see how the implementation of the CBC mode should look in practice, which helps him to understand how this type of encoding actually works.
+
+1\. \[Protocol Basics: Secure Shell Protocol - The Internet Protocol Journal, Volume 12, No.4 by William Stallings\][↩](#ref1)  
+2\. [\[https://de.wikipedia.org/wiki/Datei:CBC_decryption.svg\]](https://de.wikipedia.org/wiki/Datei:CBC_decryption.svg)[↩](#ref2)
---
+++ Source_Project/SSH_TLP/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSH_TLP
+{
+    class Program
+    {
+        #region OneBlockDES
+        public static byte[] DesEncryptOneBlock(byte[] plainText, byte[] key)
+        {
+            //Implementation of DES algorithm for one block
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            des.KeySize = 64;
+            des.Key = key;
+            des.Mode = CipherMode.ECB;
+            des.Padding = PaddingMode.None;
+
+            ICryptoTransform ic = des.CreateEncryptor();
+
+            byte[] enc = ic.TransformFinalBlock(plainText, 0, 8);
+
+            return enc;
+        }
+        #endregion
+
+        #region CreateByteArray
+        //create and initialize array of bytes
+        public static byte[] CreateByteArray(int length)
+        {
+            var arr = new byte[length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = 0x20;
+            }
+            return arr;
+        }
+        #endregion
+
+        #region PrintArrayOfBytes
+        public static void PrintArrayOfBytes(byte[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.Write("\n");
+        }
+        #endregion
+        static void Main(string[] args)
+        {
+            int pktl = 0;
+            int pdl = 0;
+            byte[] keyE = CreateByteArray(8);
+            byte[] IV = CreateByteArray(8); ;
+            string keyDES;
+
+            //Initialization of IV
+            RNGCryptoServiceProvider numIV = new RNGCryptoServiceProvider();
+            numIV.GetBytes(IV);
+
+            #region Input values
+            /***************************************************************************/
+            Console.WriteLine("\nType in a plain text: \n");
+            string inputString = Console.ReadLine();
+            /***************************************************************************/
+            do
+            {
+                Console.WriteLine("\nType in a key for DES(strictly 8 characters): \n");
+                keyDES = Console.ReadLine();
+            }
+            while (keyDES.Length != 8);
+
+            for (int i = 0; i < 8; i++)
+            {
+                keyE[i] = (byte)keyDES[i];
+            }
+            /***************************************************************************/
+            Console.WriteLine("\nType in a key for MAC(arbitrary number of characters): \n");
+            string keyForMac = Console.ReadLine();
+
+            byte[] keyMAC = CreateByteArray(keyForMac.Length);
+
+            for (int i = 0; i < keyForMac.Length; i++)
+            {
+                keyMAC[i] = (byte)keyForMac[i];
+            }
+            /***************************************************************************/
+            #endregion
+
+            //padding
+            pktl = inputString.Length;
+
+            if ((pktl + 2) % 8 != 0)
+            {
+                pdl = 8 - ((pktl + 2) % 8);
+            }
+
+            int finalLength = pktl + pdl + 2;
+            byte[] random = new byte[pdl];
+
+            RNGCryptoServiceProvider num = new RNGCryptoServiceProvider();
+            num.GetBytes(random);
+
+            byte[] payload = CreateByteArray(finalLength);
+            byte[] finalString = CreateByteArray(finalLength);
+
+            payload[0] = (byte)pktl;
+            payload[1] = (byte)pdl;
+            int k = 0;
+
+            for (int i = 2; i < finalLength; i++)
+            {
+                if (i < finalLength - pdl)
+                {
+                    payload[i] = (byte)inputString[i - 2];
+                }
+                else
+                {
+                    payload[i] = random[k];
+                    k++;
+                }
+
+            }
+
+            Console.WriteLine("The final array of bytes for encoding: ");
+            PrintArrayOfBytes(payload);
+            /***************************************************************************/
+
+            #region DES
+            //Implementation of CBC mode by using DES function for one block
+            byte[] helpString = CreateByteArray(8);
+
+            for (int i = 0; i < finalLength; i += 8)
+            {
+                for(int j = 0; j < 8; j++)
+                {
+                    helpString[j] = (byte)(payload[i + j] ^ IV[j]);
+                }
+
+                IV = DesEncryptOneBlock(helpString, keyE);
+
+
+                for (int j = 0; j < 8; j++)
+                {
+                    finalString[i + j] = IV[j];
+                }                
+
+            }
+
+            Console.WriteLine("Coded bytes: ");
+            PrintArrayOfBytes(finalString);
+            #endregion
+
+            #region HMAC
+            HMACSHA1 hmac = new HMACSHA1(keyMAC);
+            hmac.Initialize();
+
+            Console.WriteLine("HMAC value: " + BitConverter.ToString(hmac.ComputeHash(payload)).Replace("-", "").ToLower());
+
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
